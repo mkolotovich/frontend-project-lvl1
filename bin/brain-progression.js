762f8ca4425c1010353a655507a197996871f9a0
Mike <k2punk@gmail.com>
2021-02-08 14:31:09 +0300

    fix brain-progressin call

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,4 +4,4 @@ import { greeting } from '../src/cli.js';
 import { playGame } from '../src/index.js';
 
 greeting();
-playGame([5, 7, 9, 11, 13, 15, 17, 19, 21, 23], 'What number is missing in the progression?');
+playGame([], 'What number is missing in the progression?');
