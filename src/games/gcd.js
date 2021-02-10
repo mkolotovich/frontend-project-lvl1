c5935a7791e666e201125c59feb42ef3fe630cd6
Mike <k2punk@gmail.com>
2021-02-10 12:28:03 +0300

    refactor gcd function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,25 +2,19 @@ import readlineSync from 'readline-sync';
 import { whatIsUserName } from '../cli.js';
 
 const isGcd = (arr, index, index1) => {
-  const firstNum = arr[index];
-  const secondNum = arr[index1];
+  const firstNum = arr[index]; const secondNum = arr[index1];
   console.log(`Question: ${firstNum} ${secondNum}`);
-  let bigger;
-  let smaller;
+  let bigger; let smaller;
   if (firstNum > secondNum) {
-    bigger = firstNum;
-    smaller = secondNum;
+    bigger = firstNum; smaller = secondNum;
   } else {
-    bigger = secondNum;
-    smaller = firstNum;
+    bigger = secondNum; smaller = firstNum;
   }
-  let restOfDivide;
-  let quotient;
+  let restOfDivide; let quotient;
   do {
     quotient = Math.floor(bigger / smaller);
     restOfDivide = bigger - quotient * smaller;
-    bigger = smaller;
-    smaller = restOfDivide;
+    bigger = smaller; smaller = restOfDivide;
   }
   while (restOfDivide !== 0);
   const answer = readlineSync.question('Your answer: ');
