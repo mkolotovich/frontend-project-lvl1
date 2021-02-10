4e56f817887668bdc174aa1f322004dbe20363d1
Mike <k2punk@gmail.com>
2021-02-10 15:51:00 +0300

    continue refactor playGame function

--- src/index.js
+++ src/index.js
@@ -26,14 +26,12 @@ export const playGame = (arr, question) => {
     const funcArr = [isPrime, isNumInProgression, isGcd, isEven, isCalculateNumCorrect];
     const questionsArr = ['Answer "yes" if given number is prime. Otherwise answer "no".', 'What number is missing in the progression?', 'Find the greatest common divisor of given numbers.', 'Answer "yes" if the number is even, otherwise answer "no".', 'What is the result of the expression?'];
     const funcIndex = questionsArr.indexOf(question);
-    if (questionsArr.includes(question)) {
-      if (funcArr[funcIndex](arr, index, index1, index2)) {
-        correctAnswersCount += 1;
-        isCorrect = true;
-      } else {
-        correctAnswersCount = winAnswersCount;
-        isCorrect = false;
-      }
+    if (funcArr[funcIndex](arr, index, index1, index2)) {
+      correctAnswersCount += 1;
+      isCorrect = true;
+    } else {
+      correctAnswersCount = winAnswersCount;
+      isCorrect = false;
     }
   }
   if (correctAnswersCount === winAnswersCount && isCorrect === true) {
