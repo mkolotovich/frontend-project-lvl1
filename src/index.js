74e58d3cc09f09ae6ddc700b6f81f11b7f691a8c
Mike <k2punk@gmail.com>
2021-02-16 15:20:22 +0300

    remove extra variables

--- src/index.js
+++ src/index.js
@@ -45,23 +45,21 @@ const playGame = (arr, question) => {
     const questionsArr = ['Answer "yes" if given number is prime. Otherwise answer "no".', 'What number is missing in the progression?', 'Find the greatest common divisor of given numbers.', 'Answer "yes" if the number is even, otherwise answer "no".', 'What is the result of the expression?'];
     const funcIndex = questionsArr.indexOf(question);
     const signs = ['+', '-', '*'];
-    const firstNum = arr[index];
-    const secondNum = arr[index1];
-    const expression = `${firstNum} ${signs[index2]} ${secondNum}`;
+    const expression = `${arr[index]} ${signs[index2]} ${arr[index1]}`;
     const modifyedNumbers = arr.slice();
     modifyedNumbers[index] = '..';
     const progression = modifyedNumbers.join(' ');
     if (question === 'Answer "yes" if the number is even, otherwise answer "no".' || question === 'Answer "yes" if given number is prime. Otherwise answer "no".') {
       console.log(`Question: ${arr[index]}`);
     } else if (question === 'Find the greatest common divisor of given numbers.') {
-      console.log(`Question: ${firstNum} ${secondNum}`);
+      console.log(`Question: ${arr[index]} ${arr[index1]}`);
     } else if (question === 'What number is missing in the progression?') {
       console.log(`Question: ${progression}`);
     } else {
       console.log(`Question: ${expression}`);
     }
     const answer = readlineSync.question('Your answer: ');
-    if (getAnswer(funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer),
+    if (getAnswer(funcArr[funcIndex](arr, index, index1, index2, arr[index], arr[index1], answer),
       question, answer)) {
       correctAnswersCount += 1;
     } else {
