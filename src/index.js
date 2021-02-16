2141851b3d72b7553f954779884c069c8136e5ce
Mike <k2punk@gmail.com>
2021-02-16 11:50:13 +0300

    refactor condition for correct answers

--- src/index.js
+++ src/index.js
@@ -42,9 +42,8 @@ const playGame = (arr, question) => {
       console.log(`Question: ${expression}`);
     }
     const answer = readlineSync.question('Your answer: ');
-    if ((funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer)[0] && question === 'What is the result of the expression?')
-    || (funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer)[0] && question === 'Find the greatest common divisor of given numbers.')
-    || (funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer)[0] && question === 'What number is missing in the progression?')) {
+    if ((funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer)[0] && question !== 'Answer "yes" if given number is prime. Otherwise answer "no".')
+    || (funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer)[0] && question !== 'Answer "yes" if the number is even, otherwise answer "no".')) {
       correctAnswersCount += 1;
       console.log('Correct!');
     } else if (funcArr[funcIndex](arr, index, index1, index2, firstNum, secondNum, answer)[0] && answer === 'yes') {
