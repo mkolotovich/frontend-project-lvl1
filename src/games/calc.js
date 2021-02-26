bfd874e13b03584bdc45a554a7a95ed2135d2e6f
Mike <k2punk@gmail.com>
2021-02-26 12:42:45 +0300

    remove extra variable in isCalculateNumCorrect

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,13 +1,12 @@
 const isCalculateNumCorrect = (index, index1, index2, answer) => {
   const arr = [35, 16, 4, 10, 25, 11, 7];
   const question = 'What is the result of the expression?';
-  const signs = ['+', '-', '*'];
-  const actions = [(a, b) => a + b, (a, b) => a - b, (a, b) => a * b];
+  const actions = [(a, b) => a + b, (a, b) => a - b, (a, b) => a * b, ['+', '-', '*']];
   let result;
   if (index2 !== undefined) {
     result = actions[index2](arr[index], arr[index1]);
   }
-  const expression = `${arr[index]} ${signs[index2]} ${arr[index1]}`;
+  const expression = `${arr[index]} ${actions[3][index2]} ${arr[index1]}`;
   if (result === Number(answer)) {
     return [true, question, expression, arr];
   }
