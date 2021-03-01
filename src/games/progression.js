7d348601d0816b7e21296de024d301fe23384568
Mike <k2punk@gmail.com>
2021-03-01 12:20:59 +0300

    refactor progression function

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,13 +1,16 @@
-const isNumInProgression = (index, index1, index2, answer) => {
+const isNumInProgression = (num, answer) => {
+  if (num === Number(answer)) {
+    return [true];
+  }
+  return [false, num];
+};
+
+const checkProgression = (index, index1, index2, answer) => {
   const arr = [5, 7, 9, 11, 13, 15, 17, 19, 21, 23];
-  const question = 'What number is missing in the progression?';
   const modifyedNumbers = arr.slice();
   modifyedNumbers[index] = '..';
   const progression = modifyedNumbers.join(' ');
-  if (arr[index] === Number(answer)) {
-    return [true, question, progression, arr];
-  }
-  return [false, question, progression, arr, arr[index]];
+  return [isNumInProgression(arr[index], answer)[0], progression, arr, arr[index]];
 };
 
-export default isNumInProgression;
+export default checkProgression;
