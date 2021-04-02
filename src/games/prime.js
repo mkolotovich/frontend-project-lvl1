d67c4d9179f67675abedb25ff3fd875fa5f76edb
Mike <k2punk@gmail.com>
2021-04-02 15:47:50 +0300

    refactor prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,5 @@
 import playGame from '../index.js';
+import getRandomIndex from '../randomNum.js';
 
 const isPrime = (num) => {
   if (num < 2) {
@@ -12,10 +13,20 @@ const isPrime = (num) => {
   return true;
 };
 
-const checkPrime = (randomIndex) => {
+const checkPrime = (answer, randomIndexParam) => {
   const nums = [3, 5, 6, 7];
+  let randomIndex = getRandomIndex(nums);
+  let randomIndexClone;
+  if (randomIndexParam === undefined) {
+    randomIndexClone = randomIndex;
+  } else {
+    randomIndexClone = randomIndexParam;
+  }
+  if (randomIndex !== randomIndexClone) {
+    randomIndex = randomIndexClone;
+  }
   const question = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  return [isPrime(nums[randomIndex]), checkPrime, nums, question, nums[randomIndex], nums];
+  return [isPrime(nums[randomIndex]), checkPrime, nums[randomIndex], question, randomIndexClone];
 };
 
 const startGame = () => {
