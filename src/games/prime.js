98a82813125158ed7d18fd535098090020619e74
Mike <k2punk@gmail.com>
2021-04-19 15:14:32 +0300

    refactor prime game

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,11 +1,11 @@
 import playGame from '../index.js';
-import getRandomIndex from '../randomNum.js';
+import getRandomNum from '../randomNum.js';
 
 const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = 2; i < num / 2; i += 1) {
+  for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
       return false;
     }
@@ -13,25 +13,11 @@ const isPrime = (num) => {
   return true;
 };
 
-const generateGameData = (answer, randomIndexParam) => {
-  const nums = [3, 5, 6, 7];
-  let randomIndex = getRandomIndex(nums);
-  let randomIndexClone;
-  if (randomIndexParam === undefined) {
-    randomIndexClone = randomIndex;
-  } else {
-    randomIndexClone = randomIndexParam;
-  }
-  if (randomIndex !== randomIndexClone) {
-    randomIndex = randomIndexClone;
-  }
-  const result = isPrime(nums[randomIndex]);
-  const gameData = [randomIndexClone, nums[randomIndex]];
-  if ((result && answer === 'yes') || (!result && answer === 'no')) {
-    gameData.unshift(true);
-  } else {
-    gameData.unshift(false);
-  }
+const generateGameData = (answer) => {
+  const randomNum = getRandomNum(1, 10);
+  const result = isPrime(randomNum);
+  const gameData = [];
+  gameData.push(randomNum);
   if (result && answer !== 'yes') {
     gameData.push('yes');
   } else {
@@ -40,13 +26,9 @@ const generateGameData = (answer, randomIndexParam) => {
   return gameData;
 };
 
-const checkPrime = () => {
-  const question = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  return [question, generateGameData];
-};
-
 const startGame = () => {
-  playGame(checkPrime());
+  const question = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  playGame(question, generateGameData);
 };
 
 export default startGame;
