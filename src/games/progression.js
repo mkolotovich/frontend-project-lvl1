685aef8f72bf6c6b129a38253e0cd1f960d7417c
Mike <k2punk@gmail.com>
2021-04-19 13:31:57 +0300

    refactor progression game

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,43 +1,19 @@
 import playGame from '../index.js';
-import getRandomIndex from '../randomNum.js';
+import getRandomIndex from '../randomIndex.js';
 
-const isNumInProgression = (num, answer) => {
-  if (num === Number(answer)) {
-    return [true];
-  }
-  return [false, num];
-};
-
-const generateGameData = (answer, randomIndexesParam) => {
+const generateGameData = () => {
   const nums = [5, 7, 9, 11, 13, 15, 17, 19, 21, 23];
   const modifyedNumbers = nums.slice();
-  let randomIndex = getRandomIndex(nums);
-  let randomIndexClone;
-  if (randomIndexesParam === undefined) {
-    randomIndexClone = randomIndex;
-  } else {
-    randomIndexClone = randomIndexesParam;
-  }
-  if (randomIndex !== randomIndexClone) {
-    randomIndex = randomIndexClone;
-  }
+  const randomIndex = getRandomIndex(nums);
   modifyedNumbers[randomIndex] = '..';
   const progression = modifyedNumbers.join(' ');
-  const [result, correctAnswer] = isNumInProgression(nums[randomIndex], answer);
-  const gameData = [result, randomIndex, progression];
-  if (result !== Number(answer)) {
-    gameData.push(correctAnswer);
-  }
+  const gameData = [progression, `${nums[randomIndex]}`];
   return gameData;
 };
 
-const checkProgression = () => {
-  const question = 'What number is missing in the progression?';
-  return [question, generateGameData];
-};
-
 const startGame = () => {
-  playGame(checkProgression());
+  const question = 'What number is missing in the progression?';
+  playGame(question, generateGameData);
 };
 
 export default startGame;
