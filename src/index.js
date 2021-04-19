21861695c4d63ceee1002beb36f31e197482a7b8
Mike <k2punk@gmail.com>
2021-04-19 15:35:38 +0300

    remove rest operator from playGame function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const playGame = (...args) => {
+const playGame = (question, gameFunction) => {
   let correctAnswersCount = 0;
-  const [question, gameFunction] = args;
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
