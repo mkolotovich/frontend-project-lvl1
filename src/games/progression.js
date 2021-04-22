7338689c44a3dd43dc99b70b36091edea56ff625
Mike <k2punk@gmail.com>
2021-04-22 11:00:14 +0300

    refactor progression game with generator

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,8 +1,20 @@
 import playGame from '../index.js';
 import getRandomIndex from '../randomNum.js';
 
+const makeProgression = (firstItem, step, length) => {
+  const minLength = 5;
+  const progressionLength = (length < minLength) ? minLength : length;
+  const progression = [];
+  let progressionItem = firstItem;
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression[i] = progressionItem;
+    progressionItem += step;
+  }
+  return progression;
+};
+
 const generateGameData = () => {
-  const progression = [5, 7, 9, 11, 13, 15, 17, 19, 21, 23];
+  const progression = makeProgression(5, 2, 10);
   const modifyedNumbers = progression.slice();
   const randomIndex = getRandomIndex(0, progression.length);
   modifyedNumbers[randomIndex] = '..';
