9fa1d7ad861b72aadf966f4696a070514dfb6ed2
Mike <k2punk@gmail.com>
2021-04-22 12:20:53 +0300

    refactor prime game, remove extra variable

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,28 +2,15 @@ import playGame from '../index.js';
 import getRandomNum from '../randomNum.js';
 
 const isPrime = (num) => {
-  if (num < 2) {
-    return false;
-  }
-  for (let i = 2; i <= num / 2; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
+  if (num < 2) { return false; }
+  for (let i = 2; i <= num / 2; i += 1) { if (num % i === 0) { return false; } }
   return true;
 };
 
-const generateGameData = (answer) => {
-  const randomNum = getRandomNum(1, 10);
-  const result = isPrime(randomNum);
-  const gameData = [];
-  gameData.push(randomNum);
-  if (result && answer !== 'yes') {
-    gameData.push('yes');
-  } else {
-    gameData.push('no');
-  }
-  return gameData;
+const generateGameData = () => {
+  const question = getRandomNum(1, 10);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return [question, answer];
 };
 
 const startGame = () => {
